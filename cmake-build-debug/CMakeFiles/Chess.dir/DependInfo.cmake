c3ae111cde0983d4d1bad47f7ab57cca399e8b47
Zach Stoebner <zstoebner25@gmail.com>
2019-12-23 15:00:19 -0600

    restart with design patterns

---
+++ cmake-build-debug/CMakeFiles/Chess.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Zachary/Desktop/Code/C++/Chess/src/Bishop.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/cmake-build-debug/CMakeFiles/Chess.dir/src/Bishop.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/src/King.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/cmake-build-debug/CMakeFiles/Chess.dir/src/King.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/src/Knight.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/cmake-build-debug/CMakeFiles/Chess.dir/src/Knight.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/src/Pawn.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/cmake-build-debug/CMakeFiles/Chess.dir/src/Pawn.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/src/Queen.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/cmake-build-debug/CMakeFiles/Chess.dir/src/Queen.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/src/Rook.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/cmake-build-debug/CMakeFiles/Chess.dir/src/Rook.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ legacy/src/cmake-build-debug/CMakeFiles/Chess.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/Bishop.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Bishop.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/Board.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Board.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/BoardNode.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/BoardNode.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/ChessGame.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/ChessGame.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/King.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/King.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/Knight.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Knight.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/Pawn.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Pawn.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/Queen.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Queen.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/Rook.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/Rook.cpp.o"
+  "/Users/Zachary/Desktop/Code/C++/Chess/Chess/chessdriver.cpp" "/Users/Zachary/Desktop/Code/C++/Chess/Chess/cmake-build-debug/CMakeFiles/Chess.dir/chessdriver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
