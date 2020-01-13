3cb1d99c3f12b45ba1c42650e65306e66423594d
Zach Stoebner <zstoebner25@gmail.com>
2020-01-13 13:22:33 -0600

    Updated readme with current goals

--- README.md
+++ README.md
@@ -1,23 +1,24 @@
 # Chess
 Hobby chess project started summer 2019
 
-Constructed bottom-up (or top-down... whatever), 8x8 matrix that points to a dense graphical representation of the board. Nodes point to Pieces. Utilizing STL(smart pointers, sets), inheritance, and more, for practice. 
+I'm currently redoing the architecture, implementing some GoF design patterns to modularize the app.   
 
-The current primary dependencies for this project are C++ and its Standard Template Library. 
+The current primary dependencies for this project are C++ and its Standard Template Library.
 
 # Current progress:
   - Most backend skeleton code, and mechanics
-  - Definitions of all Pieces and automated Board setup and reset
+  - Definitions of all Pieces
   - Consolidated mechanics of pieces in Board class to avoid a circular inclusion problem
-  
+
 # To-do:
-  - Defining and implementing a visual interface with which to view the current game (probably just in the terminal for now             and then moving to OpenGL or, if possible, exporting some scripts to Unity for a better visual rendering of the code
+  - Translate
+  - Defining and implementing a visual interface (planning on using OpenGL)
   - Simple testing of the backend functionality
   - Rigorous testing
   - Game driver
-  
+
 # Long term:
+  - Networking
   - Support for tips and improvement interfaces
   - Strategy tutorials
   - Support for an AI opponent with scaling difficulties
-  
--- src/Bishop.cpp
+++ src/Bishop.cpp
@@ -15,6 +15,71 @@ Bishop::Bishop(PieceColor pieceColor, u_int32_t row, u_int32_t col)
     : Piece(pieceColor, row, col) {}
 
 std::set<Location> Bishop::updateValidMoves(const Board *board) {
-  std::set<Location> moves;
-  return moves;
+    std::set<Location> schema;
+
+    PcPtr ptr;
+    size_t i; //row index
+    size_t j; //column index
+    size_t pieceCount; //one piece can be in sight in any given direction,
+    // but after that she's blind (helper function will return one
+
+    //adding all down-left diagonal tiles
+    for (i = location.row+1, j = location.column-1, pieceCount = 0;
+         i < ROWS && j >= 0 && pieceCount == 0; ++i, --j) {
+
+        ptr = getPiece(i, j);
+
+        if (!ptr || ptr->getColor() != color) {
+
+            addLocation(schema,pieceCount,i,j);
+
+        }
+
+    }
+
+
+    //adding all down-right diagonal tiles
+    for (i = location.row+1, j = location.column+1, pieceCount = 0;
+         i < ROWS && j < COLUMNS && pieceCount == 0; ++i, ++j) {
+
+        ptr = getPiece(i, j);
+
+        if (!ptr || ptr->getColor() != color) {
+
+            addLocation(schema,pieceCount,i,j);
+
+        }
+
+    }
+
+    //adding all up-left diagonal tiles
+    for (i = location.row-1, j = location.column-1, pieceCount = 0;
+         i >= 0 && j >= 0 && pieceCount == 0; --i, --j) {
+
+        ptr = getPiece(i, j);
+
+        if (!ptr || ptr->getColor() != color) {
+
+            addLocation(schema,pieceCount,i,j);
+
+        }
+
+    }
+
+
+    //adding all up-right diagonal tiles
+    for (i = location.row-1, j = location.column+1, pieceCount = 0;
+         i >= 0 && j < COLUMNS && pieceCount == 0; --i, ++j) {
+
+        ptr = getPiece(i, j);
+
+        if (!ptr || ptr->getColor() != color) {
+
+            addLocation(schema,pieceCount,i,j);
+
+        }
+
+    }
+
+    return schema;
 }
